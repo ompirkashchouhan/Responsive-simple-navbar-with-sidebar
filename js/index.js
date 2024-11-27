4e6b574fc95496da88231771aa4477fccd345844
ompirkashchouhan <omsuther6@gmail.com>
2024-11-28 01:55:26 +0500

    Responsive Naviation Bar Simple With SideBar

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const sidebar = document.getElementsByClassName("_sidebar")[0];
+sidebar.style.display = "none";
+
+function showMenu() {
+  sidebar.style.display = "flex";
+};
+
+function hideMenu() {
+  sidebar.style.display = "none";
+};
